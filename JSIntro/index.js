53c5a7b64945dba00e49c61bd47612a25c7a754b
Zinnet <zinnetyazicii@gmail.com>
2022-07-18 14:05:13 +0300

    JavascriptIntro has been completed

---
+++ JSIntro/index.js
@@ -0,0 +1,19 @@
+
+    const names = ["zinnet" ,"yazici" ,"asd"];
+
+    const root = document.getElementById("root");
+    root.innerHTML ="DOM Manipülasyonu.";
+   
+ 
+
+    names.map(name => {
+        let element = document.createElement("p");
+        element.innerHTML = name;
+        root.append(element);
+        console.log(name);
+    })
+    console.log(names);
+
+
+   
+
---
+++ JSIntro/readme.md
@@ -0,0 +1,2 @@
+git init
+git switch -c <branchName>
